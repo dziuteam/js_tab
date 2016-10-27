0a882e17490b140b67894fd27846f6dae55bbcac
Przemek Pamirski <ppamirski@gmail.com>
2016-10-27 12:08:31 +0200

    Tak jak w zadaniu połączyłem 2 tablice do dodalem Mariana i warunek, zeby nie dublowac imion

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var womensNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var mensNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = womensNames.concat(mensNames);
+
+console.log(allNames);
+
+var newName ='Marian';
+if (allNames.indexOf(newName) == -1) {
+	var x = allNames.push(newName);
+} else {
+	console.log('ten czlowiek juz jest w tablicy');
+}
+console.log(allNames);
+
